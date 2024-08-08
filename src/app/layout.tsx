651c034c5c77be36df8a681f3b408d8ce18a0f92
fadilkun45 <indocrafter76@gmail.com>
2024-08-08 15:10:02 +0700

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
